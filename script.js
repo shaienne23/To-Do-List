cc34747904775e686ac31e4bb4827077f28cb2f2
shaienne23 <shaienneo@gmail.com>
2024-03-15 11:20:29 -0300

    feat: adicionando elementos

---
+++ script.js
@@ -0,0 +1,20 @@
+const createTaskForm = document.getElementById("createTaskForm")
+const taskInput = document.getElementById("taskInput")
+
+let taskList = []
+
+createTaskForm.addEventListener("submit", (e) => addTask(e))
+
+function addTask(e){
+
+    e.preventDefault()
+    const inputValue = taskInput.value
+
+    taskList.push(inputValue)
+    taskInput.value = ""
+
+    const taskDiv = document.createElement("div")
+    const taskDescription = document.createElement("p")
+
+    taskDescription.appendChild(taskDescription)
+}
